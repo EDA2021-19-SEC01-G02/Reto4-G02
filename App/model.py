9de9472f46e459ac8bc28b3af07b199f61f2f322
Mjerrabaine <85709451+Mjerrabaine@users.noreply.github.com>
2021-07-31 18:32:01 -0500

    cambios 2

--- App/model.py
+++ App/model.py
@@ -32,7 +32,7 @@ from DISClib.ADT.graph import gr
 from DISClib.ADT import map as mp
 from DISClib.DataStructures import mapentry as me
 from DISClib.Algorithms.Graphs import scc
-from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.ADT import orderedmap as om
 assert cf
 
 """
@@ -54,17 +54,32 @@ def newAnalyzer():
                     'landing': None,
                     'cableconections': None,
                     'countries': None,
-                    'paths': None
+                    'ids': None,
+                    'countriesinfo': None,
+                    'capitales': None,
+                    'components': None
                     }
 
     analyzer['landing'] = mp.newMap(numelements=14000,
                                      maptype='PROBING')
 
-    analyzer['countries'] = mp.newMap(numelements=14000,
+    analyzer['countries'] = om.newMap(omaptype='RBT',
+                                     comparefunction=compareCountries)
+
+    analyzer['capitales'] = om.newMap(omaptype='RBT',
+                                     comparefunction=compareCountries)
+    
+    analyzer['countriesinfo'] = om.newMap(omaptype='RBT',
+                                     comparefunction=compareCountries)
+
+    analyzer['ids'] = mp.newMap(numelements=14000,
                                      maptype='PROBING')
+
     analyzer['cableconections'] = gr.newGraph(datastructure='ADJ_LIST',
                                               directed=True,
-                                              size=14000)
+                                              size=14000,
+                                              comparefunction=compareVertex)
+    return analyzer
 
 # Funciones para agregar informacion al catalogo
 def addLandingPoint(analyzer, service):
@@ -79,106 +94,108 @@ def addLandingPoint(analyzer, service):
 
     Si es un cable con una ruta diferente seria: 3316-africa-coast-to-europe-ace
     """
-    format = formatVertex(service)
-    origin = format[0]
-    destination = format[1]
-    sameVertex(service)
+
+    origin = "<" + service['\ufefforigin'] + "-" + service['cable_id'] + ">"
+    destination = "<" + service['destination'] + "-" + service['cable_id'] + ">"
     length = service['cable_length']
-    addlanding(analyzer, origin)
-    addlanding(analyzer, destination)
-    addConnection(analyzer, origin, destination, length)
+    if service['\ufefforigin'] == service['destination']:
+        length = '0.100 km'
+    addGraph(analyzer, origin, destination, length)
+    addCapital(analyzer, origin, destination, length)
     return analyzer
 
-def addlanding(analyzer, cableid):
-    """
-    Adiciona un ladingpoint como un vertice del grafo
-    """
-        if not gr.containsVertex(analyzer['cableconections'], cableid):
-            gr.insertVertex(analyzer['cableconections'], cableid)
-        return analyzer
-"""
-def addRouteStop(analyzer, service):
-    """
-    #Agrega a un cable, una ruta que es servida en ese paradero
-    """
-    entry = mp.get(analyzer['landing'], service['landing_point_id'])
-    if entry is None:
-        lstlanding = lt.newList(cmpfunction=compareroutes)
-        #tambien comparo rutas?
-        lt.addLast(lstlanding, service['cable_id'])
-        mp.put(analyzer['landing'], service['landing_point_id'], lstlanding)
-    else:
-        lstlanding = entry['value']
-        info = service['cable_id']
-        if not lt.isPresent(lstlanding, info):
-            lt.addLast(lstlanding, info)
-    return analyzer
-    #para cada landing point adiciono el id del cable en la lista
-"""
-
-def addConnection(analyzer, origin, destination, cablelength):
-    """
-    Adiciona un arco entre dos landing points
-    """
-    edge = gr.getEdge(analyzer['cableconections'], origin, destination)
+def addGraph(analyzer, origen, destino, length):
+    if not gr.containsVertex(analyzer['cableconections'],origen):
+        gr.insertVertex(analyzer['cableconections'],origen)
+    if not gr.containsVertex(analyzer['cableconections'],destino):
+        gr.insertVertex(analyzer['cableconections'],destino)
+    edge = gr.getEdge(analyzer['cableconections'], origen, destino)
     if edge is None:
-        gr.addEdge(analyzer['cableconections'], origin, destination, cablelength)
+        gr.addEdge(analyzer['cableconections'],origen, destino, length)
+    return analyzer
 
+def addCapital(analyzer, origen, destino, length):
+    if mp.get(analyzer['landing'], origen) is not None and mp.get(analyzer['landing'], destino) is not None:
+        pais_origen = mp.get(analyzer['landing'], origen)['value']['pais']
+        print(pais_origen)
+        pais_destino= mp.get(analyzer['landing'], destino)['value']['pais']
+        info_origen = {'pais': pais_origen,
+                        'origen':lt.newList(),
+                        'peso': length}
+        info_destino = {'pais': pais_destino,
+                        'origen':lt.newList(),
+                        'peso': length}
+        capital_origen = om.get(analyzer['countriesinfo'], pais_origen)['value']['CapitalName']
+        capital_destino = om.get(analyzer['countriesinfo'], pais_destino)['value']['CapitalName']
+        entry = om.get(analyzer['capitales'], capital_origen)
+        if entry is None:
+            info_origen = {'pais': pais_origen,
+                        'origen':lt.newList(),
+                        'peso': length}
+            om.put(analyzer['capitales'], capital_origen, info_origen)
+        lt.addLast(entry['value']['origen'], origen)
+        if entry['value']['peso'] < length:
+            entry['value']['peso'] = length
+        
+        entrada = om.get(analyzer['capitales'], capital_destino)
+        if entrada is None:
+            info_destino = {'pais': pais_destino,
+                            'origen':lt.newList(),
+                            'peso': length}
+            om.put(analyzer['capitales'], capital_destino, info_destino)
+        lt.addLast(entrada['value']['origen'], destino)
+        if entrada['value']['peso'] < length:
+            entrada['value']['peso'] = length
 
-def formatVertex(service):
-    """
-    Se formatea el nombrer del vertice con el id de la estación
-    seguido de la ruta.
-    """
-    origen = lt.newList()
-    lt.addLast(origen,service['origin'])
-    lt.addLast(origen,service['cable_id'])
-    destino = lt.newList()
-    lt.addLast(destino,service['destination'])
-    lt.addLast(destino,service['cable_id'])
-    return origen, destino
-    """"
-    origen = service['origin'] + '-' + service['cable_id']
-    destino = service['destination'] + '-' + service['cable_id']
-    return origen,destino
-    """
+def conectarCapitales(analyzer):
+    llaves = om.keySet(analyzer['capitales'])
+    for key in llaves:
+        if om.get(analyzer['capitales'], key) is not None:
+            entry = om.get(analyzer['capitales'], key)['value']
+            for point in entry['origen']:
+                if not gr.containsVertex(analyzer['cableconections'],key):
+                    gr.insertVertex(analyzer['cableconections'],key)
+                edge = gr.getEdge(analyzer['cableconections'], key, point)
+                if edge is None:
+                    gr.addEdge(analyzer['cableconections'],key, point, entry['peso'])
+                edge1 = gr.getEdge(analyzer['cableconections'], point, key)
+                if edge1 is None:
+                    gr.addEdge(analyzer['cableconections'],point, key, entry['peso'])
+
     #en otro mapa guardar los ids como llaves y como valor el string service["origin"] y service["service[cable id"]
-    
 
-def sameVertex(service):
-    """
-    En caso de que el archivo tenga un espacio en la
-    distancia, se reemplaza con cero.
-    """
-    if service['origin'] == service['destination']:
-        service['cable_length'] = "0.100 km"
 
 def loadlandings(analyzer, landing):
-    mp.put(analyzer['landing'], landing["landing_point_id"], landing)
-    pais= landing["name"].split(', ')[1].lower()
+    entry = mp.get(analyzer['landing'], landing['landing_point_id'])
+    pais= landing['name'].split(",")
+    if len(pais) == 1:
+        info = pais[0].strip()
+    else:
+        info = pais[1].strip()
     idlanding = landing["landing_point_id"]
-    addCountry(analyzer, pais , idlanding)
-
-     
-
+    if entry is None:
+        addinformacion = {'pais':info,
+                                'informacion': landing}
+        mp.put(analyzer['landing'], landing['landing_point_id'], addinformacion)
+    addCountry(analyzer, info , idlanding)
+ 
 def addCountry(analyzer, country, idlanding):
     paises = analyzer['countries']
-    if mp.contains(paises, country):
-        value = me.getValue(mp.get(paises, country))
-        lt.addLast(value, idlanding)
-    else:
+    entry = om.get(paises, country)
+    if entry is None:
         lista = lt.newList()
         lt.addLast(lista, idlanding)
-    mp.put(paises, country, lista)
+        om.put(paises, country, lista)
+    else:
+        lt.addLast(entry['value'], idlanding)
+    #para asegurarme que solo este agregando
+    #lo cambie a un order map 
 
-def loadCapital(analyzer, capital):
-    gr.insertVertex(analyzer['cableconections'], capital["CapitalName"])
-    paises = analyzer['countries']
-    country = capital["CountryName"].lower()
-    if mp.contains(paises, country):
-        value = me.getValue(mp.get(paises, country))
-
-
+def loadCountries(analyzer, country):
+    entry = om.get(analyzer['countriesinfo'], country['CountryName'])
+    if entry is None:
+        om.put(analyzer['countriesinfo'], country['CountryName'], country)
+    return analyzer
 
 
 # Funciones para creacion de datos
@@ -193,19 +210,76 @@ def connected(analyzer):
     analyzer['vertex'] = scc.KosarajuSCC(analyzer['cableconections'])
     return scc.connectedComponents(analyzer['vertex'])
 
-def totalStops(analyzer):
+def landingPoints(analyzer):
     """
     Retorna el total de landing points (vertices) del grafo
     """
-    return gr.numVertices(analyzer['landing'])
+    return gr.numVertices(analyzer['cableconections'])
 
 
 def totalConnections(analyzer):
     """
     Retorna el total arcos del grafo
     """
-    return gr.numEdges(analyzer['landing'])
+    return gr.numEdges(analyzer['cableconections'])
+
+def totalCountries(analyzer):
+    return om.size(analyzer['countries'])
+
+def informacionLanding(analyzer):
+    primero = mp.get(analyzer['landing'], '3316')
+    informacion = primero['value']['informacion']
+    return informacion
+
+def informacionCountries(analyzer):
+    primero = om.get(analyzer['countriesinfo'], 'Chuuk')
+    informacion = primero['value']
+    return informacion
+
+def connectedComponents(analyzer):
+    """
+    Calcula los componentes conectados del grafo
+    Se utiliza el algoritmo de Kosaraju
+    """
+    analyzer['components'] = scc.KosarajuSCC(analyzer['cableconections'])
+    return scc.connectedComponents(analyzer['components'])
+
+def sameCluster(analyzer, origen, destino):
+    adyacentes = gr.adjacents(analyzer['cableconections'],origen)
+    relacion = False
+    if lt.isPresent(adyacentes, destino):
+        relacion = True
+    return relacion
+
+"""
+def sameCluster(analyzer, origen, destino):
+    vertice1 = gr.containsVertex(analyzer['cableconections'], origen)
+    vertice2 = gr.containsVertex(analyzer['cableconections'], destino)
+    if vertice1 is False and vertice2 is False:
+        return "0"
+    elif vertice1 is False:
+        return "1"
+    elif vertice2 is False:
+        return "2"
+    else:
+        return scc.stronglyConnected(analyzer['cableconections'], origen, destino)
+"""
 
 # Funciones utilizadas para comparar elementos dentro de una lista
+def compareVertex(trip1,trip2):
+    if (trip1 == trip2['key']):
+        return 0
+    elif (trip1 > trip2['key']):
+        return 1
+    else:
+        return -1
+    #compara las llaves
+def compareCountries(c1,c2):
+    if (c1 == c2):
+        return 0
+    elif (c1 > c2):
+        return 1
+    else:
+        return -1
 
 # Funciones de ordenamiento
